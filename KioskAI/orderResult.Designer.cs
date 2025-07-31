609586b7a20ad535ee2ea2ea4d3fa50914e600b8
DH.L <meozigoon@gmail.com>
2025-07-31 20:40:29 +0900

    orderResult size auto flex implemented

--- KioskAI/orderResult.Designer.cs
+++ KioskAI/orderResult.Designer.cs
@@ -28,56 +28,45 @@
             panelMain.Controls.Add(CancelButton_);
             panelMain.Dock = DockStyle.Fill;
             panelMain.Location = new Point(0, 0);
-            panelMain.Margin = new Padding(4);
             panelMain.Name = "panelMain";
             panelMain.Size = new Size(625, 707);
-            panelMain.TabIndex = 0;
             // 
             // orderResultDataGridView
             // 
             orderResultDataGridView.AllowUserToAddRows = false;
-            orderResultDataGridView.ColumnHeadersHeight = 34;
+            orderResultDataGridView.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            orderResultDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            orderResultDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            orderResultDataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
             orderResultDataGridView.Location = new Point(12, 12);
-            orderResultDataGridView.Margin = new Padding(4);
             orderResultDataGridView.Name = "orderResultDataGridView";
             orderResultDataGridView.RowHeadersWidth = 62;
-            orderResultDataGridView.SelectionMode = DataGridViewSelectionMode.CellSelect;
             orderResultDataGridView.Size = new Size(600, 562);
-            orderResultDataGridView.TabIndex = 1;
             // 
             // totalLabel
             // 
             totalLabel.AutoSize = true;
             totalLabel.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
             totalLabel.Location = new Point(12, 588);
-            totalLabel.Margin = new Padding(4, 0, 4, 0);
             totalLabel.Name = "totalLabel";
-            totalLabel.Size = new Size(95, 28);
-            totalLabel.TabIndex = 2;
             totalLabel.Text = "총액: 0원";
             // 
             // OrderButton
             // 
             OrderButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             OrderButton.Location = new Point(417, 656);
-            OrderButton.Margin = new Padding(4);
             OrderButton.Name = "OrderButton";
             OrderButton.Size = new Size(94, 38);
-            OrderButton.TabIndex = 3;
             OrderButton.Text = "결제하기";
-            OrderButton.UseVisualStyleBackColor = true;
             OrderButton.Click += OrderButton_Click;
             // 
-            // CancelButton
+            // CancelButton_
             // 
             CancelButton_.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             CancelButton_.Location = new Point(518, 656);
-            CancelButton_.Margin = new Padding(4);
             CancelButton_.Name = "CancelButton";
             CancelButton_.Size = new Size(94, 38);
-            CancelButton_.TabIndex = 4;
             CancelButton_.Text = "취소";
-            CancelButton_.UseVisualStyleBackColor = true;
             CancelButton_.Click += CancelButton_Click;
             // 
             // orderResult
@@ -87,7 +76,6 @@
             ClientSize = new Size(625, 707);
             Controls.Add(panelMain);
             FormBorderStyle = FormBorderStyle.FixedDialog;
-            Margin = new Padding(4);
             MaximizeBox = false;
             Name = "orderResult";
             Text = "WISH Order";
