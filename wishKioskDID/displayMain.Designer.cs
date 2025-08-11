f09ab1490a411c036ae153380b7e352026d7ff11
DH.L <meozigoon@gmail.com>
2025-08-11 22:19:29 +0900

    docs demo img changed
    Kiosk text display style edited
    
    Kiosk serverURL edit validation check added

--- wishKioskDID/displayMain.Designer.cs
+++ wishKioskDID/displayMain.Designer.cs
@@ -45,10 +45,10 @@ namespace wishKioskDIDDisplay
             // 
             groupBoxCompleted.Controls.Add(flowLayoutPanelCompletedOrders);
             groupBoxCompleted.Dock = DockStyle.Left;
-            groupBoxCompleted.Font = new System.Drawing.Font("맑은 고딕", 30F);
+            groupBoxCompleted.Font = new System.Drawing.Font("맑은 고딕", 40F);
             groupBoxCompleted.Location = new Point(0, 0);
             groupBoxCompleted.Name = "groupBoxCompleted";
-            groupBoxCompleted.Size = new Size(365, 723);
+            groupBoxCompleted.Size = new Size(397, 723);
             groupBoxCompleted.TabIndex = 1;
             groupBoxCompleted.TabStop = false;
             groupBoxCompleted.Text = "완료";
@@ -58,9 +58,9 @@ namespace wishKioskDIDDisplay
             flowLayoutPanelCompletedOrders.AutoScroll = true;
             flowLayoutPanelCompletedOrders.Dock = DockStyle.Fill;
             flowLayoutPanelCompletedOrders.FlowDirection = FlowDirection.TopDown;
-            flowLayoutPanelCompletedOrders.Location = new Point(3, 83);
+            flowLayoutPanelCompletedOrders.Location = new Point(3, 110);
             flowLayoutPanelCompletedOrders.Name = "flowLayoutPanelCompletedOrders";
-            flowLayoutPanelCompletedOrders.Size = new Size(359, 637);
+            flowLayoutPanelCompletedOrders.Size = new Size(391, 610);
             flowLayoutPanelCompletedOrders.TabIndex = 0;
             flowLayoutPanelCompletedOrders.WrapContents = false;
             // 
@@ -68,10 +68,10 @@ namespace wishKioskDIDDisplay
             // 
             groupBoxPreparing.Controls.Add(flowLayoutPanelOrders);
             groupBoxPreparing.Dock = DockStyle.Fill;
-            groupBoxPreparing.Font = new System.Drawing.Font("맑은 고딕", 30F);
-            groupBoxPreparing.Location = new Point(365, 0);
+            groupBoxPreparing.Font = new System.Drawing.Font("맑은 고딕", 40F);
+            groupBoxPreparing.Location = new Point(397, 0);
             groupBoxPreparing.Name = "groupBoxPreparing";
-            groupBoxPreparing.Size = new Size(764, 723);
+            groupBoxPreparing.Size = new Size(732, 723);
             groupBoxPreparing.TabIndex = 2;
             groupBoxPreparing.TabStop = false;
             groupBoxPreparing.Text = "주문 준비중";
@@ -81,9 +81,9 @@ namespace wishKioskDIDDisplay
             flowLayoutPanelOrders.AutoScroll = true;
             flowLayoutPanelOrders.Dock = DockStyle.Fill;
             flowLayoutPanelOrders.FlowDirection = FlowDirection.TopDown;
-            flowLayoutPanelOrders.Location = new Point(3, 83);
+            flowLayoutPanelOrders.Location = new Point(3, 110);
             flowLayoutPanelOrders.Name = "flowLayoutPanelOrders";
-            flowLayoutPanelOrders.Size = new Size(758, 637);
+            flowLayoutPanelOrders.Size = new Size(726, 610);
             flowLayoutPanelOrders.TabIndex = 0;
             flowLayoutPanelOrders.WrapContents = false;
             // 
--- wishKioskDIDReceive/receiveMain.Designer.cs
+++ wishKioskDIDReceive/receiveMain.Designer.cs
@@ -55,10 +55,10 @@
             // 
             groupBoxCompleted.Controls.Add(flowLayoutPanelCompletedOrders);
             groupBoxCompleted.Dock = DockStyle.Left;
-            groupBoxCompleted.Font = new Font("맑은 고딕", 30F);
+            groupBoxCompleted.Font = new Font("맑은 고딕", 35F);
             groupBoxCompleted.Location = new Point(0, 106);
             groupBoxCompleted.Name = "groupBoxCompleted";
-            groupBoxCompleted.Size = new Size(250, 617);
+            groupBoxCompleted.Size = new Size(360, 617);
             groupBoxCompleted.TabIndex = 1;
             groupBoxCompleted.TabStop = false;
             groupBoxCompleted.Text = "완료";
@@ -68,9 +68,9 @@
             flowLayoutPanelCompletedOrders.AutoScroll = true;
             flowLayoutPanelCompletedOrders.Dock = DockStyle.Fill;
             flowLayoutPanelCompletedOrders.FlowDirection = FlowDirection.TopDown;
-            flowLayoutPanelCompletedOrders.Location = new Point(3, 83);
+            flowLayoutPanelCompletedOrders.Location = new Point(3, 97);
             flowLayoutPanelCompletedOrders.Name = "flowLayoutPanelCompletedOrders";
-            flowLayoutPanelCompletedOrders.Size = new Size(244, 531);
+            flowLayoutPanelCompletedOrders.Size = new Size(354, 517);
             flowLayoutPanelCompletedOrders.TabIndex = 0;
             flowLayoutPanelCompletedOrders.WrapContents = false;
             // 
@@ -78,10 +78,10 @@
             // 
             groupBoxPreparing.Controls.Add(flowLayoutPanelOrders);
             groupBoxPreparing.Dock = DockStyle.Fill;
-            groupBoxPreparing.Font = new Font("맑은 고딕", 30F);
-            groupBoxPreparing.Location = new Point(250, 106);
+            groupBoxPreparing.Font = new Font("맑은 고딕", 35F);
+            groupBoxPreparing.Location = new Point(360, 106);
             groupBoxPreparing.Name = "groupBoxPreparing";
-            groupBoxPreparing.Size = new Size(879, 617);
+            groupBoxPreparing.Size = new Size(769, 617);
             groupBoxPreparing.TabIndex = 2;
             groupBoxPreparing.TabStop = false;
             groupBoxPreparing.Text = "주문 준비중";
@@ -91,9 +91,9 @@
             flowLayoutPanelOrders.AutoScroll = true;
             flowLayoutPanelOrders.Dock = DockStyle.Fill;
             flowLayoutPanelOrders.FlowDirection = FlowDirection.TopDown;
-            flowLayoutPanelOrders.Location = new Point(3, 83);
+            flowLayoutPanelOrders.Location = new Point(3, 97);
             flowLayoutPanelOrders.Name = "flowLayoutPanelOrders";
-            flowLayoutPanelOrders.Size = new Size(873, 531);
+            flowLayoutPanelOrders.Size = new Size(763, 517);
             flowLayoutPanelOrders.TabIndex = 0;
             flowLayoutPanelOrders.WrapContents = false;
             // 
